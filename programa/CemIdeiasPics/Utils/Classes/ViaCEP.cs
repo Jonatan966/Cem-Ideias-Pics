7b489cb2302e6a78462ef1230a1fd404d8f1cb27
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-04-19 20:29:35 -0300

    Criação da classe ViaCEP
    Classe responsável por consumir a API do ViaCEP, com o objetivo de pesquisar um CEP e apresentar os dados do endereço

---
+++ programa/CemIdeiasPics/Utils/Classes/ViaCEP.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+namespace CemIdeiasPics.Utils.Classes
+{
+    public static class ViaCEP
+    {
+        private static string URL { get; } = "http://cep.republicavirtual.com.br/web_cep.php?cep={0}&formato=json";
+        public static CEP BuscarCEP(string CEP)
+        {
+            var result = string.Empty;
+            using (var webClient = new WebClient())
+            {
+                result = webClient.DownloadString(string.Format(URL, CEP));
+            }
+            return JsonConvert.DeserializeObject<CEP>(result);
+        }
+
+        public class CEP
+        {
+            [JsonProperty("resultado")]
+            public string Resultado { get; set; }
+
+            [JsonProperty("resultado_txt")]
+            public string ResultadoTxt { get; set; }
+
+            [JsonProperty("uf")]
+            public string Uf { get; set; }
+
+            [JsonProperty("cidade")]
+            public string Cidade { get; set; }
+
+            [JsonProperty("bairro")]
+            public string Bairro { get; set; }
+
+            [JsonProperty("tipo_logradouro")]
+            public string TipoLogradouro { get; set; }
+
+            [JsonProperty("logradouro")]
+            public string Logradouro { get; set; }
+        }
+    }
+}
