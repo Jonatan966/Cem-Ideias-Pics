1c12cb09cff1c784ad9911f9410cb601ad85d6fb
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-04-19 21:41:40 -0300

    Adicionada a classe Mensagens
    Classse responsável por cuidar das mensagens predefinidas que serão mostradas ao usuário

---
+++ programa/CemIdeiasPics/Utils/Classes/Mensagens.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CemIdeiasPics.Utils.Classes
+{
+    public static class Mensagens
+    {
+        private static string[] Tipos { get; } = 
+        { 
+            "Aviso!", 
+            "Erro!", 
+            "Confirmação" 
+        };
+        private static MessageBoxIcon[] Icones { get; } = 
+        { 
+            MessageBoxIcon.Warning, 
+            MessageBoxIcon.Error, 
+            MessageBoxIcon.Asterisk 
+        };
+        public enum MensagensPredefinidas 
+        {
+            RESULTADO_NAO_ENCONTRADO = 0
+        }
+        private static string[] Predefinidos { get; } =
+        {
+            "1§O resultado da busca não foi encontrado."
+        };
+
+        public static DialogResult MostrarMensagem(MensagensPredefinidas mensagem)
+        {
+            string[] msg = Predefinidos[(int)mensagem].Split('§');
+            int type = int.Parse(msg[0]);
+            return MessageBox.Show(msg[1], Tipos[type], MessageBoxButtons.OK, Icones[type]);
+        }
+    }
+}
