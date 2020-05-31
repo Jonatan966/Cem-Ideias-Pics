ef7e84ba0b1c6ac399917587ed312edca4015073
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-05-31 16:16:44 -0300

    Método BuscarCEP alterado para assíncrono

--- programa/CemIdeiasPics/Utils/Classes/ViaCEP.cs
+++ programa/CemIdeiasPics/Utils/Classes/ViaCEP.cs
@@ -1,16 +1,18 @@
 ﻿using Newtonsoft.Json;
 using System.Net;
+using System.Threading.Tasks;
+
 namespace CemIdeiasPics.Utils.Classes
 {
     public static class ViaCEP
     {
         private static string URL { get; } = "http://cep.republicavirtual.com.br/web_cep.php?cep={0}&formato=json";
-        public static CEP BuscarCEP(string CEP)
+        public static async Task<CEP> BuscarCEP(string CEP)
         {
             var result = string.Empty;
             using (var webClient = new WebClient())
             {
-                result = webClient.DownloadString(string.Format(URL, CEP));
+                await Task.Run(()=>result = webClient.DownloadString(string.Format(URL, CEP)));
             }
             return JsonConvert.DeserializeObject<CEP>(result);
         }
