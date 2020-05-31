a7293384f7c5dd2ebb0f726501196905d1b900f6
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-05-31 15:37:34 -0300

    Resolução de uma exceção
    Resolvido o erro: "O servidor remoto retornou um erro: (403) Proibido."

--- programa/CemIdeiasPics/Utils/Classes/Servidor.cs
+++ programa/CemIdeiasPics/Utils/Classes/Servidor.cs
@@ -13,19 +13,29 @@ namespace CemIdeiasPics.Utils.Classes
         public static string urlAddress = "http://cemideias-pics.000webhostapp.com/src/php/ConexaoMySQL.php";
         private protected static string pwd = "7fe182bcddb67905fbd39db957e23116";
 
+
         public static async Task<string> EnviarComandoSQL(string comando)
         {
             string pagesource = "false";
             try
             {
-               using (WebClient client = new WebClient())
+                using (WebClient client = new WebClient())
                 {
                     NameValueCollection postData = new NameValueCollection()
                     {
                             { "acesso", pwd },  //order: {"parameter name", "parameter value"}
                             { "cmd", comando }
                     };
-                    pagesource = Encoding.UTF8.GetString(await Task.Run(() => client.UploadValues(urlAddress, postData)));
+                    pagesource = Encoding.UTF8.GetString(await Task.Run(() => {
+                        try
+                        {
+                            return client.UploadValues(urlAddress, postData);
+                        }
+                        catch
+                        {
+                            return new byte[] { };
+                        }
+                    }));
                     return pagesource;
                 }
             }
