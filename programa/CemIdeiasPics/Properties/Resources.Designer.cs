48592e9eeb1ede899aaf533afc21408dde83d114
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-05-31 15:38:54 -0300

    Adição de uma imagem de perfil padrão

--- programa/CemIdeiasPics/Properties/Resources.Designer.cs
+++ programa/CemIdeiasPics/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace CemIdeiasPics.Properties {
         /// <summary>
         ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _default {
+            get {
+                object obj = ResourceManager.GetObject("default", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Camera_Icone {
             get {
                 object obj = ResourceManager.GetObject("Camera-Icone", resourceCulture);
