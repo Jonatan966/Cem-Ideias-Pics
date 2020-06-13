76bb054f280fc300956a513cb92b62e8a2121208
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-06-13 16:03:41 -0300

    Adição da classe Cliente
    Classe responsável por armazenar os valores da consulta de clientes do banco de dados

---
+++ programa/CemIdeiasPics/Utils/Consultas/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CemIdeiasPics.Utils.Consultas
+{
+    public partial class Cliente
+    {
+        [JsonProperty("CLICPF")]
+        public string CLICPF { get; set; }
+        [JsonProperty("CLINOME")]
+        public string CLINOME { get; set; }
+        [JsonProperty("CLISEXO")]
+        public string CLISEXO { get; set; }
+        [JsonProperty("CLIDATANASCIMENTO")]
+        public string CLIDATANASCIMENTO { get; set; }
+        [JsonProperty("CLITELEFONE")]
+        public string CLITELEFONE { get; set; }
+        [JsonProperty("CLIEMAIL")]
+        public string CLIEMAIL { get; set; }
+        [JsonProperty("CLINUMEROCASA")]
+        public string CLINUMEROCASA { get; set; }
+        [JsonProperty("CLICEP")]
+        public string CLICEP { get; set; }
+    }
+}
