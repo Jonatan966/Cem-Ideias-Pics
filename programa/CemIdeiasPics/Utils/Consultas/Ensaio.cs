e6e3e29e7b0fbe832f149a8fd606dddeeebf4ff7
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-06-14 18:32:17 -0300

    Adição da classe Ensaio
    Classe responsável por armazenar o conteudo das consultas relacionadas a ensaios feitas no banco de dados

---
+++ programa/CemIdeiasPics/Utils/Consultas/Ensaio.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CemIdeiasPics.Utils.Consultas
+{
+    class Ensaio
+    {
+        [JsonProperty("ENSID")]
+        public string Ensid { get; set; }
+
+        [JsonProperty("ENSCLIENTE")]
+        public string Enscliente { get; set; }
+
+        [JsonProperty("ENSUSUARIO")]
+        public string Ensusuario { get; set; }
+
+        [JsonProperty("ENSTIPO")]
+        public string Enstipo { get; set; }
+
+        [JsonProperty("ENSCEP")]
+        public string Enscep { get; set; }
+
+        [JsonProperty("ENSNUMLOCAL")]
+        public string Ensnumlocal { get; set; }
+
+        [JsonProperty("ENSDATA")]
+        public DateTimeOffset Ensdata { get; set; }
+
+        [JsonProperty("ENSPRECO")]
+        public string Enspreco { get; set; }
+
+        [JsonProperty("ENSDIRETORIO")]
+        public string Ensdiretorio { get; set; }
+    }
+}
