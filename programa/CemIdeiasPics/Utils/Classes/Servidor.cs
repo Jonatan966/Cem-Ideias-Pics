26bba137e6a5ca508ed7438350dfae4de8ac9144
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-06-19 18:13:34 -0300

    Criação do sistema de envio e recebimento de imagens do servidor

--- programa/CemIdeiasPics/Utils/Classes/Servidor.cs
+++ programa/CemIdeiasPics/Utils/Classes/Servidor.cs
@@ -50,5 +50,56 @@ namespace CemIdeiasPics.Utils.Classes
                 return pagesource;
             }
         }
+
+        public class PortifolioLoader
+        {
+            public Portifolio[] portifolio;
+            
+            public async Task CarregaImagens(string ensaioID)
+            {
+                string response = await Servidor.EnviarItem("", $"{ensaioID}|recebe", TipoEnvio.Imagem);
+                portifolio = JsonConvert.DeserializeObject<Portifolio[]>(response);
+                if (portifolio.Length > 0)
+                {
+                    CarregaImagem(0);
+                }
+            }
+
+            public Image CarregaImagem(int pos)
+            {
+                if (portifolio.Length > 0)
+                    return Manipuladores.ConverteBase64ParaImagem(portifolio[pos].Imagem);
+                else return (Properties.Resources.none);
+            }
+
+            public async Task AdicionaImagem(string filename, string ensaioID)
+            {
+                string img = Manipuladores.ConverteImagemParaBase64(filename);
+                await Servidor.EnviarItem(img, $"{ensaioID}|envia", TipoEnvio.Imagem);
+                await CarregaImagens(ensaioID);
+            }
+
+            public async Task ExcluiImagem(int img, string ensaioID, bool carregaImgs = true)
+            {
+                if (portifolio.Length > 0)
+                {
+                    if (bool.Parse(await Servidor.EnviarItem(portifolio[img].NomeImagem, $"{ensaioID}|exclui", TipoEnvio.Imagem)))
+                    {
+                        Mensagens.MostrarMensagem(MensagensPredefinidas.OPERACAO_CONCLUIDA);
+                        if (carregaImgs) await CarregaImagens(ensaioID);
+                    }
+                    else
+                    {
+                        Mensagens.MostrarMensagem(MensagensPredefinidas.ERRO_INESPERADO);
+                    }
+                }
+            }
+
+            public async Task AlteraImagem(int imgAtual, string novaImg, string ensaioID)
+            {
+                await ExcluiImagem(imgAtual, ensaioID, false);
+                await AdicionaImagem(novaImg, ensaioID);
+            }
+        }
     }
 }
---
+++ programa/CemIdeiasPics/Utils/Consultas/Portifolio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CemIdeiasPics.Utils.Consultas
+{
+    public class Portifolio
+    {
+        [JsonProperty("nomeImagem")]
+        public string NomeImagem { get; set; }
+        [JsonProperty("imagem")]
+        public string Imagem { get; set; }
+    }
+}
