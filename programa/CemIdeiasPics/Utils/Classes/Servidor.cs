d42c64b24eff67cf994413fe3c01c2d99caa00a9
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-07-01 01:11:09 -0300

    Fragmentação da classe Servidor

--- programa/CemIdeiasPics/Utils/Classes/Servidor.cs
+++ programa/CemIdeiasPics/Utils/Classes/Servidor.cs
@@ -8,55 +8,17 @@ using System.Net;
 using CemIdeiasPics.Utils.Consultas;
 using Newtonsoft.Json;
 using System.Drawing;
+using CemIdeiasPics.Classes.Online;
 
 namespace CemIdeiasPics.Utils.Classes
 {
-    public enum TipoEnvio { ComandoSQL, Imagem}
-    class Servidor
-    {
-        public static string urlAddress = "http://cemideias-pics.000webhostapp.com/src/php/";
-        private protected static string pwd = "7fe182bcddb67905fbd39db957e23116";
-        private static string[] tipoEnvioPg = new string[] { "ConexaoMySQL", "EnviaImagem" };
-
-        public static async Task<string> EnviarItem(string item, string opcional = "", TipoEnvio tipoEnvio = TipoEnvio.ComandoSQL)
-        {
-            string pagesource = "false";
-            try
-            {
-                using (WebClient client = new WebClient())
-                {
-                    NameValueCollection postData = new NameValueCollection()
-                    {
-                            { "acesso", pwd },  //order: {"parameter name", "parameter value"}
-                            { "cmd", item },
-                            { "opt", opcional }
-                    };
-                    pagesource = Encoding.UTF8.GetString(await Task.Run(() => {
-                        try
-                        {
-                            return client.UploadValues(urlAddress+tipoEnvioPg[(int)tipoEnvio]+".php", postData);
-                        }
-                        catch
-                        {
-                            return new byte[] { };
-                        }
-                    }));
-                    return pagesource;
-                }
-            }
-            catch
-            {
-                return pagesource;
-            }
-        }
-
         public class PortifolioLoader
         {
             public Portifolio[] portifolio;
             
             public async Task CarregaImagens(string ensaioID)
             {
-                string response = await EnviarItem("", $"{ensaioID}|recebe", TipoEnvio.Imagem);
+                string response = await ConectaServidor.EnviarItem("", $"{ensaioID}|recebe", TipoEnvio.Imagem);
                 portifolio = JsonConvert.DeserializeObject<Portifolio[]>(response);
                 if (portifolio.Length > 0)
                 {
