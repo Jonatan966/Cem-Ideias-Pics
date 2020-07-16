5adaf682d9f63e16338d3745a2f79651b793c6cc
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-07-16 19:53:06 -0300

    Implementação do design do formulário de configurações

--- programa/CemIdeiasPics/Formulários/Menus/MenuConfigs.Designer.cs
+++ programa/CemIdeiasPics/Formulários/Menus/MenuConfigs.Designer.cs
@@ -28,10 +28,261 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnGerenciarUsuarios = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCadTipoAlbum = new System.Windows.Forms.Button();
+            this.btnCadTipoEnsaio = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnExportarBackup = new System.Windows.Forms.Button();
+            this.btnImportarBackup = new System.Windows.Forms.Button();
+            this.btnLimparTudo = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.btnAlteraAPI = new System.Windows.Forms.Button();
+            this.btnViewLog = new System.Windows.Forms.Button();
+            this.btnSobre = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnGerenciarUsuarios);
+            this.groupBox1.Location = new System.Drawing.Point(15, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(533, 64);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controle de Usuários";
+            // 
+            // btnGerenciarUsuarios
+            // 
+            this.btnGerenciarUsuarios.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGerenciarUsuarios.BackColor = System.Drawing.Color.Black;
+            this.btnGerenciarUsuarios.FlatAppearance.BorderSize = 0;
+            this.btnGerenciarUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGerenciarUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGerenciarUsuarios.ForeColor = System.Drawing.Color.Yellow;
+            this.btnGerenciarUsuarios.Location = new System.Drawing.Point(8, 21);
+            this.btnGerenciarUsuarios.Margin = new System.Windows.Forms.Padding(5);
+            this.btnGerenciarUsuarios.Name = "btnGerenciarUsuarios";
+            this.btnGerenciarUsuarios.Size = new System.Drawing.Size(517, 35);
+            this.btnGerenciarUsuarios.TabIndex = 12;
+            this.btnGerenciarUsuarios.Text = "Gerenciar Usuários";
+            this.btnGerenciarUsuarios.UseVisualStyleBackColor = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btnCadTipoEnsaio);
+            this.groupBox2.Controls.Add(this.btnCadTipoAlbum);
+            this.groupBox2.Location = new System.Drawing.Point(15, 91);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(15);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(533, 64);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Outros Cadastros";
+            // 
+            // btnCadTipoAlbum
+            // 
+            this.btnCadTipoAlbum.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCadTipoAlbum.BackColor = System.Drawing.Color.Black;
+            this.btnCadTipoAlbum.FlatAppearance.BorderSize = 0;
+            this.btnCadTipoAlbum.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadTipoAlbum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadTipoAlbum.ForeColor = System.Drawing.Color.Yellow;
+            this.btnCadTipoAlbum.Location = new System.Drawing.Point(8, 21);
+            this.btnCadTipoAlbum.Margin = new System.Windows.Forms.Padding(5);
+            this.btnCadTipoAlbum.Name = "btnCadTipoAlbum";
+            this.btnCadTipoAlbum.Size = new System.Drawing.Size(253, 35);
+            this.btnCadTipoAlbum.TabIndex = 12;
+            this.btnCadTipoAlbum.Text = "Tipos de Álbum";
+            this.btnCadTipoAlbum.UseVisualStyleBackColor = false;
+            // 
+            // btnCadTipoEnsaio
+            // 
+            this.btnCadTipoEnsaio.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnCadTipoEnsaio.BackColor = System.Drawing.Color.Black;
+            this.btnCadTipoEnsaio.FlatAppearance.BorderSize = 0;
+            this.btnCadTipoEnsaio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadTipoEnsaio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadTipoEnsaio.ForeColor = System.Drawing.Color.Yellow;
+            this.btnCadTipoEnsaio.Location = new System.Drawing.Point(271, 21);
+            this.btnCadTipoEnsaio.Margin = new System.Windows.Forms.Padding(5);
+            this.btnCadTipoEnsaio.Name = "btnCadTipoEnsaio";
+            this.btnCadTipoEnsaio.Size = new System.Drawing.Size(254, 35);
+            this.btnCadTipoEnsaio.TabIndex = 13;
+            this.btnCadTipoEnsaio.Text = "Tipos de Ensaio";
+            this.btnCadTipoEnsaio.UseVisualStyleBackColor = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.btnLimparTudo);
+            this.groupBox3.Controls.Add(this.btnExportarBackup);
+            this.groupBox3.Controls.Add(this.btnImportarBackup);
+            this.groupBox3.Location = new System.Drawing.Point(15, 167);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(15);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(533, 109);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Integridade dos Dados";
+            // 
+            // btnExportarBackup
+            // 
+            this.btnExportarBackup.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnExportarBackup.BackColor = System.Drawing.Color.Black;
+            this.btnExportarBackup.FlatAppearance.BorderSize = 0;
+            this.btnExportarBackup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExportarBackup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExportarBackup.ForeColor = System.Drawing.Color.Yellow;
+            this.btnExportarBackup.Location = new System.Drawing.Point(271, 21);
+            this.btnExportarBackup.Margin = new System.Windows.Forms.Padding(5);
+            this.btnExportarBackup.Name = "btnExportarBackup";
+            this.btnExportarBackup.Size = new System.Drawing.Size(254, 35);
+            this.btnExportarBackup.TabIndex = 13;
+            this.btnExportarBackup.Text = "Exportar Backup";
+            this.btnExportarBackup.UseVisualStyleBackColor = false;
+            // 
+            // btnImportarBackup
+            // 
+            this.btnImportarBackup.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnImportarBackup.BackColor = System.Drawing.Color.Black;
+            this.btnImportarBackup.FlatAppearance.BorderSize = 0;
+            this.btnImportarBackup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImportarBackup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImportarBackup.ForeColor = System.Drawing.Color.Yellow;
+            this.btnImportarBackup.Location = new System.Drawing.Point(8, 21);
+            this.btnImportarBackup.Margin = new System.Windows.Forms.Padding(5);
+            this.btnImportarBackup.Name = "btnImportarBackup";
+            this.btnImportarBackup.Size = new System.Drawing.Size(253, 35);
+            this.btnImportarBackup.TabIndex = 12;
+            this.btnImportarBackup.Text = "Importar Backup";
+            this.btnImportarBackup.UseVisualStyleBackColor = false;
+            // 
+            // btnLimparTudo
+            // 
+            this.btnLimparTudo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLimparTudo.BackColor = System.Drawing.Color.DarkRed;
+            this.btnLimparTudo.FlatAppearance.BorderSize = 0;
+            this.btnLimparTudo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimparTudo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimparTudo.ForeColor = System.Drawing.Color.Yellow;
+            this.btnLimparTudo.Location = new System.Drawing.Point(8, 66);
+            this.btnLimparTudo.Margin = new System.Windows.Forms.Padding(5);
+            this.btnLimparTudo.Name = "btnLimparTudo";
+            this.btnLimparTudo.Size = new System.Drawing.Size(516, 35);
+            this.btnLimparTudo.TabIndex = 14;
+            this.btnLimparTudo.Text = "Limpar todas as informações";
+            this.btnLimparTudo.UseVisualStyleBackColor = false;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox4.Controls.Add(this.btnAlteraAPI);
+            this.groupBox4.Controls.Add(this.btnViewLog);
+            this.groupBox4.Location = new System.Drawing.Point(15, 288);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(15);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(533, 64);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Outras Configurações";
+            // 
+            // btnAlteraAPI
+            // 
+            this.btnAlteraAPI.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnAlteraAPI.BackColor = System.Drawing.Color.Black;
+            this.btnAlteraAPI.FlatAppearance.BorderSize = 0;
+            this.btnAlteraAPI.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAlteraAPI.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlteraAPI.ForeColor = System.Drawing.Color.Yellow;
+            this.btnAlteraAPI.Location = new System.Drawing.Point(271, 21);
+            this.btnAlteraAPI.Margin = new System.Windows.Forms.Padding(5);
+            this.btnAlteraAPI.Name = "btnAlteraAPI";
+            this.btnAlteraAPI.Size = new System.Drawing.Size(254, 35);
+            this.btnAlteraAPI.TabIndex = 13;
+            this.btnAlteraAPI.Text = "Alterar origem da API";
+            this.btnAlteraAPI.UseVisualStyleBackColor = false;
+            // 
+            // btnViewLog
+            // 
+            this.btnViewLog.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnViewLog.BackColor = System.Drawing.Color.Black;
+            this.btnViewLog.FlatAppearance.BorderSize = 0;
+            this.btnViewLog.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnViewLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnViewLog.ForeColor = System.Drawing.Color.Yellow;
+            this.btnViewLog.Location = new System.Drawing.Point(8, 21);
+            this.btnViewLog.Margin = new System.Windows.Forms.Padding(5);
+            this.btnViewLog.Name = "btnViewLog";
+            this.btnViewLog.Size = new System.Drawing.Size(253, 35);
+            this.btnViewLog.TabIndex = 12;
+            this.btnViewLog.Text = "Visualizar Log do sistema";
+            this.btnViewLog.UseVisualStyleBackColor = false;
+            // 
+            // btnSobre
+            // 
+            this.btnSobre.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSobre.BackColor = System.Drawing.Color.Black;
+            this.btnSobre.FlatAppearance.BorderSize = 0;
+            this.btnSobre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSobre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSobre.ForeColor = System.Drawing.Color.Yellow;
+            this.btnSobre.Location = new System.Drawing.Point(5, 370);
+            this.btnSobre.Margin = new System.Windows.Forms.Padding(5);
+            this.btnSobre.Name = "btnSobre";
+            this.btnSobre.Size = new System.Drawing.Size(553, 35);
+            this.btnSobre.TabIndex = 14;
+            this.btnSobre.Text = "Sobre a aplicação";
+            this.btnSobre.UseVisualStyleBackColor = false;
+            // 
+            // MenuConfigs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnSobre);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MenuConfigs";
+            this.Size = new System.Drawing.Size(563, 410);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGerenciarUsuarios;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCadTipoEnsaio;
+        private System.Windows.Forms.Button btnCadTipoAlbum;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnExportarBackup;
+        private System.Windows.Forms.Button btnImportarBackup;
+        private System.Windows.Forms.Button btnLimparTudo;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button btnAlteraAPI;
+        private System.Windows.Forms.Button btnViewLog;
+        private System.Windows.Forms.Button btnSobre;
     }
 }
