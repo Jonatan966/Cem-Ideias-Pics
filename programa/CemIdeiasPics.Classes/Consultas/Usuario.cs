86899b07394852775ff5d65c640e6e46fbd4bc8a
Jonatan Frederico <jonatanfrederico@gmail.com>
2020-07-17 01:48:15 -0300

    Adição de mais campos na classe Usuario

--- programa/CemIdeiasPics.Classes/Consultas/Usuario.cs
+++ programa/CemIdeiasPics.Classes/Consultas/Usuario.cs
@@ -11,12 +11,26 @@ namespace CemIdeiasPics.Classes.Consultas
     {
         [JsonProperty("USUID")]
         public string USUID { get; set; }
+        [JsonProperty("USUCPF")]
+        public string USUCPF { get; set; }
 
         [JsonProperty("USUNOME")]
         public string USUNOME { get; set; }
+        [JsonProperty("USUSEXO")]
+        public string USUSEXO { get; set; }
+        [JsonProperty("USUTELEFONE")]
+        public string USUTELEFONE { get; set; }
+        [JsonProperty("USULOGIN")]
+        public string USULOGIN { get; set; }
+        [JsonProperty("USUBLOQUEIO")]
+        public string USUBLOQUEIO { get; set; }
+        [JsonProperty("USUEMAIL")]
+        public string USUEMAIL { get; set; }
 
         [JsonProperty("USUIMG")]
         public string USUIMG { get; set; }
+        [JsonProperty("USUTIPO")]
+        public string USUTIPO { get; set; }
     }
 
 }
